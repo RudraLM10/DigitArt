26cc12d3fc835f156a6ad495b24cf0c88f600937
Rudra <rudralm10@gmail.com>
2024-07-26 19:27:56 +0530

    Till Slider done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,14 @@ import { Icons } from './Icons';
 import NavItems from './NavItems';
 import { buttonVariants } from './ui/button';
 import Cart from './Cart';
+import { getServerSideUser } from '@/lib/payload-utils';
+import { cookies } from 'next/headers';
+import UserAccountNav from './UserAccountNav';
+import MobileNav from './MobileNav';
 
-const Navbar = () => {
-  const user = null;
+const Navbar = async () => {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -14,12 +19,14 @@ const Navbar = () => {
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
-              {/* TODO :Mobile Nav */}
+              <MobileNav />
+
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
                   <Icons.logo className="h-10 w-10" />
                 </Link>
               </div>
+
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
               </div>
@@ -29,26 +36,28 @@ const Navbar = () => {
                   {user ? null : (
                     <Link
                       href="/sign-in"
-                      className={buttonVariants({ variant: 'ghost' })}
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}
                     >
                       Sign in
                     </Link>
                   )}
 
-                  {user ? (
-                    'null'
-                  ) : (
+                  {user ? null : (
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   )}
 
                   {user ? (
-                    <p></p>
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
-                      className={buttonVariants({ variant: 'ghost' })}
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}
                     >
-                      Create Account
+                      Create account
                     </Link>
                   )}
 
@@ -65,7 +74,7 @@ const Navbar = () => {
                     </div>
                   )}
 
-                  <div className=" ml-4 flow-root lg:ml-6">
+                  <div className="ml-4 flow-root lg:ml-6">
                     <Cart />
                   </div>
                 </div>
